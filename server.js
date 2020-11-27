3f44ab3959f8defbc2950300a63a1ecfc85ec640
Alyssa Marielle Raqueno <44278215+alyssaraq98@users.noreply.github.com>
2020-11-27 14:44:14 -0500

    add files to repo

---
+++ server.js
@@ -0,0 +1,38 @@
+// These are our required libraries to make the server work.
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import express from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.route('/api')
+  .get((req, res) => {
+    console.log('GET request detected');
+    res.send(`Lab 5 for ${process.env.NAME}`);
+  })
+  .post(async (req, res) => {
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    console.log('POST request detected');
+    const json = await data.json();
+    console.log('Fetch request data', data);
+    res.json(json);
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
