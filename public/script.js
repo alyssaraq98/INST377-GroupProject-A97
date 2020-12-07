2d19a8cf37a742fc36da25fc4a58e8fb3a5439fe
Andrew Carroll <54873125+andcar23@users.noreply.github.com>
2020-12-07 14:44:17 -0500

    Server Set-Up
    Added Button on index.html that shows the JSON formatted data

---
+++ public/script.js
@@ -0,0 +1,19 @@
+  document.body.addEventListener('submit', async (e) => {
+    e.preventDefault(); // this stops whatever the browser wanted to do itself.
+    const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
+    fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form)
+    })
+      .then((fromServer) => fromServer.json())
+      .then((fromServer) => {
+        if (document.querySelector('.flex-inner')) {
+          document.querySelector('.flex-inner').remove();
+        }    
+        console.log('fromServer', fromServer);
+      })
+      .catch((err) => console.log(err));
+  });
